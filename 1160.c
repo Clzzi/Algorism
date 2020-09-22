4f6643e8138a3e878b644a320e57270b5c9abe8e
Clzzi <sonmin1407@dgsw.hs.kr>
2020-09-22 21:56:54 +0900

    1160 - Code UP

---
+++ 1160.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int a  =0;
+	scanf("%d",&a);
+	if ( a == 1 || a == 3)
+	printf("oh my god");
+	else if ( a == 5 || a == 7)
+	printf("oh my god");
+	else
+	printf("enjoy");
+}
+
