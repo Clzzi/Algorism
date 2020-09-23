aca05651ad1a6c83060db0c7117412028d180ab0
Clzzi <sonmin1407@dgsw.hs.kr>
2020-09-23 22:14:08 +0900

    1207 - Code up

---
+++ 1207.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c,d =0;
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    if (a+b+c+d==1)
+    printf("µµ");
+    else if (a+b+c+d==2)
+    printf("°³");
+    else if (a+b+c+d==3)
+    printf("°É");
+    else if (a+b+c+d==4)
+    printf("À·");
+    else
+    printf("¸ð");
+}
+
