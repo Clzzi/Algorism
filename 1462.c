352540885f83fe35f9db8c2268a2bdc11596aa3d
Clzzi <sonmin1407@dgsw.hs.kr>
2020-11-07 23:33:20 +0900

    1462 - Code Up

---
+++ 1462.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+	int n = 0;
+	int i,j=0;
+	int cnt = 1;
+	int arr[101][101];
+	scanf("%d",&n);
+	for(i = 0; i < n;i++) {
+		for(j = 0 ; j < n ; j ++) {
+			arr[j][i] = cnt;
+			cnt++;
+		}
+	}
+	for(i = 0; i < n;i++) {
+		for(j = 0 ; j < n ; j ++) {
+			printf("%d ",arr[i][j]);
+		}
+		printf("\n");
+	}
+	
+}
